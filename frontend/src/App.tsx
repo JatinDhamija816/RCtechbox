517f8f21e6d81a976e3fc2ac070fc9d0ab540d2f
jatin dhamija <jdhammija816@gmail.com>
2024-11-07 12:43:47 +0530

    name

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,9 @@ import Task from "./components/Task";
 const App = () => {
   return (
     <div className="bg-gray-800 min-h-screen p-5 font-serif ">
+      <div className="text-center text-white text-2xl font-mono p-5">
+        RCtechbox
+      </div>
       <div className="md:max-w-screen-md md:mx-auto md:my-10">
         <Home />
         <Task />
