fcdba4c7a0f482955ea31769032edd3324e94130
jatin dhamija <jdhammija816@gmail.com>
2024-11-07 12:37:06 +0530

    bug1

--- server/src/app.ts
+++ server/src/app.ts
@@ -6,7 +6,7 @@ import taskRoute from './routes/task';
 const app = express()
 
 const corsOptions = {
-    origin: ['http://localhost:5173', 'https://r-ctechbox.vercel.app/'],
+    origin: ['http://localhost:5173', 'https://r-ctechbox.vercel.app'],
     credentials: true,
 };
 
